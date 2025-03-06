dc8aee01e422e15eb20029c5fd5f2447c89b9113
Roberto <roberto.c.m.s123ef@gmail.com>
2025-03-06 15:15:52 -0600

    actualizacion del archivo inicio

--- inicio.py
+++ inicio.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
+import mediapipe as mp
+import serial
+import math
+import time
+import numpy as np
+import threading
