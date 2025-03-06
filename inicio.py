1037065957c095cd6ef79bf151fb356448193d9a
Roberto <roberto.c.m.s123ef@gmail.com>
2025-03-06 15:22:40 -0600

    restauracion de inicio

--- inicio.py
+++ inicio.py
@@ -1,9 +1,1 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-import cv2
-import mediapipe as mp
-import serial
-import math
-import time
-import numpy as np
-import threading
+print("hola mundo")
